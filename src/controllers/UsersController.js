9212d48395f87ff3e79a38e501e061175ac386c6
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-01-25 16:27:46 -0300

    new user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,6 +3,7 @@ const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
 const sqliteConnection = require('../database/sqlite');
+const e = require("express");
 
 class UsersController {
    
@@ -25,6 +26,39 @@ class UsersController {
 
     return response.status(201).json();
    
+  }
+
+  async update(request, response){
+    const {name, email} = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM WHERE id = (?)", [id]);
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== id){
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+
+      [user.name, user.email,new Date(), id]
+      );
+
+      return response.json();
   }
 }
 
