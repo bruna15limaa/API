c9c9895010438060057ae9d5e71c5f0e1cc24c3d
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-01-26 19:38:15 -0300

    upload

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,4 +1,7 @@
-const { Router } = require('express')
+const { Router, request, response } = require('express');
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 const UsersController = require("../controllers/UsersController");
 
@@ -10,5 +13,9 @@ const usersController = new UsersController();
 
 usersRoutes.post('/', usersController.create);
 usersRoutes.put("/:id", usersController.update);
+usersRoutes.patch("/avatar",ensureAuthenticated, (request, response) => {
+    console.log(request.file.filename);
+    response.json
+});
 
 module.exports = usersRoutes
