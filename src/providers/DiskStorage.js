6581f23f8ae330ecc1546ae76a4594ba235f7093
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-01-27 12:39:16 -0300

    upload

--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -1,5 +1,5 @@
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
 const uploadConfig = require("../configs/upload");
 
 class DiskStorage {
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,16 +6,16 @@ const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
-
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
 
 
 usersRoutes.post('/', usersController.create);
 usersRoutes.put("/:id", usersController.update);
-usersRoutes.patch("/avatar",ensureAuthenticated, (request, response) => {
+usersRoutes.patch("/avatar",ensureAuthenticated, upload.single("avatar"), (request,  response) => {
     console.log(request.file.filename);
-    response.json
+    response.json();
 });
 
 module.exports = usersRoutes
